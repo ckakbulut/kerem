0dcfdcf526f1275ee64c7c8503dabf46fb96982c
Can Kerem Akbulut <zcabcka@ucl.ac.uk>
2023-11-15 12:37:00 +0300

    created new components and refactored files

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
 import React from "react";
-import logo from "./logo.svg";
+import SidebarNav from "./components/SidebarNav";
+import Intro from "./components/Intro";
+import Experience from "./components/Experience";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 import "./App.css";
-import Typist from "react-typist";
+import "./styles/Global.css";
 
 function App() {
   return (
     <div className="App">
-      <Typist avgTypingDelay={120}>
-        <span className="intro-title">
-          {"hi, "}
-          <span className="intro-name">{"kerem"}</span>
-          {" here."}
-        </span>
-      </Typist>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="content">
+        <Intro />
+        <About />
+        <Experience />
+        <Projects />
+        <Contact />
+        <Footer />
+      </div>
+      <SidebarNav />
     </div>
   );
 }
