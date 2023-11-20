7b39302164b2888e21b5f3fda197de2cde882e04
Can Kerem Akbulut <zcabcka@ucl.ac.uk>
2023-11-20 13:41:43 +0000

    added credits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Footer() {
-  return <></>;
+    return (
+        <div id="footer">
+            <div className="credits">
+                <div>
+                    Built by Can Kerem Akbulut, design inspired from Gazi Jarin.
+                </div>
+                <div>All rights reserved. ©</div>
+            </div>
+        </div>
+    );
 }
 
 export default Footer;
