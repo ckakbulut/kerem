b38a1a0713755aca209046d627d05d224e650d77
ckakbulut <cankeremakbulut@gmail.com>
2024-06-05 20:24:06 +0300

    Changed intro message and description

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,20 +2,23 @@ import React from "react";
 import Typist from "react-typist";
 import "react-typist/dist/Typist.css";
 import EmailRoundedIcon from "@material-ui/icons/EmailRounded";
+import "../styles/Intro.css";
 
 function Intro() {
     return (
         <div id="intro">
             <Typist avgTypingDelay={150}>
-                <Typist.Delay ms={1500} />
-                {"Hi, "}
-                <span className="intro-name">{"Kerem"} </span>
-                {" here."}
+                <span className="intro-title">
+                    <Typist.Delay ms={1500} />
+                    {"Hi, "}
+                    {"I'm "}
+                    <span className="intro-name">{"Kerem."} </span>
+                </span>
             </Typist>
-            <div classname="intro-subtitle">
+            <div className="intro-subtitle">
                 ex-@UCL | currently creating things and working on some stuff
             </div>
-            <div classname="intro-description">
+            <div className="intro-desc">
                 I'm a software engineer based part-time in Istanbul and
                 part-time in London. I'm interested in building websites,
                 learning about artificial intelligence and studying computer
