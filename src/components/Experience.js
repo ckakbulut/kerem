505fee33cc657a07608abe0c168222f4dd13e463
ckakbulut <cankeremakbulut@gmail.com>
2024-06-05 20:22:06 +0300

    added experience section with job list

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,7 +1,19 @@
 import React from "react";
+import JobList from "./JobList";
+import "../styles/Experience.css";
+import FadeInSection from "./FadeInSection";
 
-function Experience() {
-  return <></>;
-}
+const Experience = () => {
+    return (
+        <div id="experience">
+            <FadeInSection>
+                <div className="section-header">
+                    <span className="section-title">/ experience</span>
+                </div>
+                <JobList />
+            </FadeInSection>
+        </div>
+    );
+};
 
 export default Experience;
